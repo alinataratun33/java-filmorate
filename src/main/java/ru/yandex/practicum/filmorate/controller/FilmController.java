055576954844d1f0342d694bc7c58445ca4fb01b
Alina <taratunalina@yandex.by>
2025-12-07 19:04:26 +0300

    Выполнить спринт 10

--- src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
@@ -1,7 +1,89 @@
 package ru.yandex.practicum.filmorate.controller;
 
-import org.springframework.web.bind.annotation.RestController;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.*;
+import ru.yandex.practicum.filmorate.exception.ValidationException;
+import ru.yandex.practicum.filmorate.model.Film;
+
+import java.time.LocalDate;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
+@RequestMapping("/films")
+@Slf4j
 public class FilmController {
+
+    private final Map<Long, Film> films = new HashMap<>();
+    private static final LocalDate MIN_RELEASE_DATE = LocalDate.of(1895, 12, 28);
+    private static final int MAX_DESCRIPTION_LENGTH = 200;
+
+    @GetMapping
+    public Collection<Film> getAllFilms() {
+        log.debug("Получен запрос на получение всех фильмов");
+        return films.values();
+    }
+
+    @PostMapping
+    public Film addFilm(@RequestBody Film film) {
+        log.debug("Получен запрос на добавление фильма");
+        validateFilm(film);
+        film.setId(getNextId());
+        films.put(film.getId(), film);
+        log.info("Фильм с ID {} успешно добавлен. Название: '{}'", film.getId(), film.getName());
+        return film;
+    }
+
+    @PutMapping
+    public Film updateFilm(@RequestBody Film newFilm) {
+        log.debug("Получен запрос на обновление информации о фильме с ID" + newFilm.getId());
+        if (newFilm.getId() == null) {
+            log.error("Попытка обновления фильма без указания ID");
+            throw new ValidationException("ID не может быть пустым");
+        }
+        if (!films.containsKey(newFilm.getId())) {
+            log.warn("Попытка обновления несуществующего пользователя с ID: {}", newFilm.getId());
+            throw new ValidationException("Фильм с ID " + newFilm.getId() + " не найден");
+        }
+        validateFilm(newFilm);
+
+        Film oldFilm = films.get(newFilm.getId());
+        oldFilm.setName(newFilm.getName());
+        oldFilm.setDescription(newFilm.getDescription());
+        oldFilm.setDuration(newFilm.getDuration());
+        oldFilm.setReleaseDate(newFilm.getReleaseDate());
+        log.info("Фильм с ID {} успешно обновлен", newFilm.getId());
+        return oldFilm;
+    }
+
+    private void validateFilm(Film film) {
+        log.debug("Начата валидация фильма {}", film);
+        if (film.getName() == null || film.getName().isBlank()) {
+            log.debug("Валидация не прошла: название фильма пустое");
+            throw new ValidationException("Название не может быть пустым");
+        }
+        if (film.getDescription().length() > MAX_DESCRIPTION_LENGTH) {
+            log.debug("Валидация не прошла: лимит символов превышен");
+            throw new ValidationException("Описание не может быть больше " + MAX_DESCRIPTION_LENGTH + " символов");
+        }
+        if (film.getReleaseDate().isBefore((MIN_RELEASE_DATE))) {
+            log.debug("Валидация не прошла: дата релиза указана неверно");
+            throw new ValidationException("Дата релиза не может быть раньше " + MIN_RELEASE_DATE);
+        }
+        if (film.getDuration() <= 0) {
+            log.debug("Валидация не прошла: продолжительность фильма отприцательная или равна 0");
+            throw new ValidationException("Продолжительность фильма не может быть отрицательным числом или 0");
+        }
+        log.debug("Валидация фильма прошла успешно");
+    }
+
+    private long getNextId() {
+        long currentMaxId = films.keySet()
+                .stream()
+                .mapToLong(id -> id)
+                .max()
+                .orElse(0);
+        return ++currentMaxId;
+    }
 }
---
+++ src/test/java/ru/yandex/practicum/filmorate/FilmControllerTest.java
@@ -0,0 +1,124 @@
+package ru.yandex.practicum.filmorate;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import ru.yandex.practicum.filmorate.controller.FilmController;
+import ru.yandex.practicum.filmorate.exception.ValidationException;
+import ru.yandex.practicum.filmorate.model.Film;
+
+import java.time.LocalDate;
+import java.util.Collection;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class FilmControllerTest {
+    private FilmController filmController;
+
+    @BeforeEach
+    void setUp() {
+        filmController = new FilmController();
+    }
+
+    @Test
+    void testFilmWithEmptyName() {
+        Film film = new Film();
+        film.setName("");
+        film.setDescription("Description");
+        film.setReleaseDate(LocalDate.of(2000, 1, 1));
+        film.setDuration(120);
+
+        ValidationException exception = assertThrows(ValidationException.class, () -> {
+            filmController.addFilm(film);
+        });
+        assertEquals("Название не может быть пустым", exception.getMessage());
+    }
+
+    @Test
+    void testFilmDescription() {
+        Film film = new Film();
+        film.setName("Film");
+        film.setDescription("A".repeat(201));
+        film.setReleaseDate(LocalDate.of(2000, 1, 1));
+        film.setDuration(120);
+
+        ValidationException exception = assertThrows(ValidationException.class, () -> {
+            filmController.addFilm(film);
+        });
+        assertEquals("Описание не может быть больше 200 символов", exception.getMessage());
+    }
+
+    @Test
+    void testFilmRealiseDate() {
+        Film film = new Film();
+        film.setName("Film");
+        film.setDescription("Description");
+        film.setReleaseDate(LocalDate.of(1822, 1, 1));
+        film.setDuration(120);
+
+        ValidationException exception = assertThrows(ValidationException.class, () -> {
+            filmController.addFilm(film);
+        });
+        assertEquals("Дата релиза не может быть раньше 1895-12-28", exception.getMessage());
+    }
+
+    @Test
+    void testFilmDuration() {
+        Film film = new Film();
+        film.setName("Film");
+        film.setDescription("Description");
+        film.setReleaseDate(LocalDate.of(2000, 1, 1));
+        film.setDuration(-1);
+
+        ValidationException exception = assertThrows(ValidationException.class, () -> {
+            filmController.addFilm(film);
+        });
+        assertEquals("Продолжительность фильма не может быть отрицательным числом или 0", exception.getMessage());
+    }
+
+    @Test
+    void testAddCorrectFilm() {
+        Film film = new Film();
+        film.setName("Film");
+        film.setDescription("Description");
+        film.setReleaseDate(LocalDate.of(2000, 1, 1));
+        film.setDuration(120);
+
+        Film addedFilm = filmController.addFilm(film);
+        assertNotNull(addedFilm.getId(), "Фильм должен получить ID");
+        assertEquals("Film", addedFilm.getName());
+        assertEquals(120, addedFilm.getDuration());
+        assertEquals(LocalDate.of(2000, 1, 1), addedFilm.getReleaseDate());
+
+        Collection<Film> allFilms = filmController.getAllFilms();
+        assertEquals(1, allFilms.size());
+    }
+
+    @Test
+    void testUpdateCorrectFilm() {
+        Film film = new Film();
+        film.setName("Film");
+        film.setDescription("Description");
+        film.setReleaseDate(LocalDate.of(2000, 1, 1));
+        film.setDuration(120);
+
+        Film addedFilm = filmController.addFilm(film);
+        Long filmId = addedFilm.getId();
+
+        Film updatedData = new Film();
+        updatedData.setId(filmId);
+        updatedData.setName("Updated Name");
+        updatedData.setDescription("Updated Description");
+        updatedData.setReleaseDate(LocalDate.of(2001, 1, 1));
+        updatedData.setDuration(150);
+
+        Film updatedFilm = filmController.updateFilm(updatedData);
+
+        assertEquals(filmId, updatedFilm.getId(), "ID должен остаться прежним");
+        assertEquals("Updated Name", updatedFilm.getName());
+        assertEquals("Updated Description", updatedFilm.getDescription());
+        assertEquals(LocalDate.of(2001, 1, 1), updatedFilm.getReleaseDate());
+        assertEquals(150, updatedFilm.getDuration());
+
+        assertEquals(1, filmController.getAllFilms().size());
+    }
+}
